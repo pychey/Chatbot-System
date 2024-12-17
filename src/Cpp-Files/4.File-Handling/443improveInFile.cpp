069b990978212285e7263693c6e93fa5633194d7
pychey <prakpycheyy@gmail.com>
2024-12-17 22:56:19 +0700

    done file improve delete

--- src/Cpp-Files/4.File-Handling/443improveInFile.cpp
+++ src/Cpp-Files/4.File-Handling/443improveInFile.cpp
@@ -1,5 +1,23 @@
 #include "../../Header-Files/Chatbot.hpp" 
 
 void Chatbot::improveInFile(string questionToImprove,string newAnswer) {
-    //
+    string question;
+    string answer;
+    bool found = false;
+    ifstream file(filePath);
+    ofstream tempFile(fileTemp, ios::app);
+    while(getline(file,question)){
+        getline(file,answer);
+        if(question == questionToImprove){
+            tempFile << questionToImprove << endl;
+            tempFile << newAnswer << endl;
+        } else {
+            tempFile << question << endl;
+            tempFile << answer << endl;
+        }
+    }
+    file.close();
+    tempFile.close();
+    remove("QA.txt");
+    rename("temp.txt","QA.txt");
 }
--- src/Cpp-Files/4.File-Handling/444deleteFromFile.cpp
+++ src/Cpp-Files/4.File-Handling/444deleteFromFile.cpp
@@ -1,5 +1,28 @@
 #include "../../Header-Files/Chatbot.hpp"
 
 void Chatbot::deleteFromFile(string questionForDeleted) {
-    //
+    string question;
+    string answer;
+    ifstream file(filePath);
+    ofstream tempFile(fileTemp, ios::app);
+    if(file.is_open()){
+        while(getline(file,question)){
+            getline(file,answer);
+            if(question==questionForDeleted) {
+                 //skip the question to be deleted
+            } else {
+                tempFile << question << endl;
+                tempFile << answer << endl;
+            }
+        }
+        file.close();
+        tempFile.close();
+
+        //for some reason, it doesnt accept string filePath and fileTemp
+        remove("QA.txt");
+        rename("temp.txt","QA.txt");
+    } else {
+        cout << "File opened unsuccessfully" << endl;
+        return;
+    }
 }
